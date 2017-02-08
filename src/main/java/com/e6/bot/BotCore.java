ced295821e652f551d5f5142bfff41efe9265af7
Justin Schlabach <jschlabach@everesttech.com>
2017-02-08 15:55:12 -0500

    Updated the core to auto reconnect when it loses connection to the server

--- src/main/java/com/e6/bot/BotCore.java
+++ src/main/java/com/e6/bot/BotCore.java
@@ -4,31 +4,55 @@ import com.e6.bot.config.Configuration;
 import com.github.theholywaffle.teamspeak3.TS3Api;
 import com.github.theholywaffle.teamspeak3.TS3Config;
 import com.github.theholywaffle.teamspeak3.TS3Query;
+import com.github.theholywaffle.teamspeak3.api.reconnect.ConnectionHandler;
+import com.github.theholywaffle.teamspeak3.api.reconnect.ReconnectStrategy;
 import com.github.theholywaffle.teamspeak3.api.wrapper.Client;
+
+import java.util.logging.Level;
 
 class BotCore implements Runnable {
 
     private Thread thread;
-    private final TS3Api api;
-    private final TS3Query query;
+    private TS3Api api;
+    private TS3Query query;
     private boolean running = true;
-    private Configuration appConfig;
+    private static Configuration appConfig;
 
     BotCore () {
+
         // Configuration file that allows users to edit the bot's behavior.
         appConfig = new Configuration();
 
         // Bot connection config.
         final TS3Config config = new TS3Config();
         config.setHost(appConfig.getServerAddress());
-        //config.setDebugLevel(Level.ALL);
+        config.setDebugLevel(Level.ALL);
+
+        // Reconnect timer gets longer and longer.
+        config.setReconnectStrategy(ReconnectStrategy.exponentialBackoff());
+
+        // Reconnection logic
+        config.setConnectionHandler(new ConnectionHandler() {
+            @Override
+            public void onConnect(TS3Query ts3Query) {
+                loginLogic(ts3Query.getApi());
+            }
+
+            @Override
+            public void onDisconnect(TS3Query ts3Query) {
+                // Nada
+            }
+        });
 
         // Connecting to the server's query engine with the set configuration.
         query = new TS3Query(config);
         query.connect();
 
-        // Initializing the api call handler and logging in.
-        api = query.getApi();
+        loginLogic(query.getApi());
+    }
+
+    private void loginLogic(TS3Api tsApi) {
+        api = tsApi;
         api.login(appConfig.getUsername(), appConfig.getPassword());
         api.selectVirtualServerById(appConfig.getVirtualServerId());
         api.setNickname(appConfig.getBotNickname());
@@ -37,20 +61,25 @@ class BotCore implements Runnable {
     @Override
     public void run() {
         while (running) {
-            for (Client c : api.getClients()) {
-                if (c.getIdleTime() >= appConfig.getAfkTimeToMove()) {
-                    if (c.getChannelId() != 8) {
-                        System.out.println(c.getNickname() + " has been AFK to long and is being moved...");
-                        api.moveClient(c.getId(), 8);
-                    }
-                }
-                //int[] groups = c.getServerGroups();
-            }
             try {
-                Thread.sleep(10000);
+                for (Client c : api.getClients()) {
+                    if (c.getIdleTime() >= appConfig.getAfkTimeToMove()) {
+                        if (c.getChannelId() != appConfig.getAfkChannelId()) {
+                            System.out.println(c.getNickname() + " has been AFK to long and is being moved...");
+                            api.moveClient(c.getId(), appConfig.getAfkChannelId());
+                        }
+                    }
+                    //int[] groups = c.getServerGroups();
+                }
+                try {
+                    Thread.sleep(appConfig.getBotCheckDelay());
+                } catch (InterruptedException ex) {
+                    // This is an expected exception since we interrupt the thread when it is sleeping to exit the app.
+                }
             }
-            catch (InterruptedException ex) {
-                // This is an expected exception since we interrupt the thread when it is sleeping to exit the app.
+            catch (NullPointerException ex) {
+                ex.printStackTrace();
+                System.out.println("Likely connection issues...");
             }
         }
     }
--- src/main/java/com/e6/bot/config/Configuration.java
+++ src/main/java/com/e6/bot/config/Configuration.java
@@ -11,11 +11,16 @@ public class Configuration {
     private String username = "TestAdmin";
     private String password = "USbS3jbC";
     private int virtualServerId = 1;
+    private int afkChannelId = 8;
     private long afkTimeToMove = 30000;
+    private long botCheckDelay = 10000;
     private int[] safeGroups;
 
     public int getVirtualServerId() {
         return virtualServerId;
+    }
+    public int getAfkChannelId() {
+        return afkChannelId;
     }
     public String getServerAddress() {
         return serverAddress;
@@ -32,6 +37,9 @@ public class Configuration {
     public long getAfkTimeToMove() {
         return afkTimeToMove;
     }
+    public long getBotCheckDelay() {
+        return botCheckDelay;
+    }
     public int[] getSafeGroups() {
         return safeGroups;
     }
@@ -45,9 +53,11 @@ public class Configuration {
             serverAddress = config.getString("serverAddress");
             username = config.getString("username");
             password = config.getString("password");
-            afkTimeToMove = Long.parseLong(config.getString("afkTimeToMove"));
+            afkChannelId = config.getInt("afkChannelId");
             virtualServerId = config.getInt("virtualServerId");
             botNickname = config.getString("botNickname");
+            afkTimeToMove = config.getLong("afkTimeToMove");
+            botCheckDelay = config.getLong("botCheckDelay");
 
             List<String> safeGroupList = config.getList("safeGroups");
             safeGroups = new int[safeGroupList.size()];
